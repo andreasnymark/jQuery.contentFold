0b9d71e9dbf482b2559762f22ed0043077ff5ea2
Andreas Nymark <andreas@ibuypink.com>
2013-09-21 14:51:59 +0200

    Added viewport init size.

--- jquery.contentFold.js
+++ jquery.contentFold.js
@@ -14,52 +14,69 @@
 		**
 		*/
 		var defaults = {  
+			initSize: 0,
 		    element: 'h2',
 		    classCollapsed: 'is-collapsed',
 		    classExpanded: 'is-opened',
 		    classContent: 'content-fold'
 		};  
 		var options = $.extend(defaults, options); 
-		return this.each(function() { 
-			/*
-			**
-			** wrapper to start from, find element, add class for collapsed content.
-			** for all siblings following the element, we wrap it in a div-element.
-			*/
-			$(this)
-				.find(options.element)
-				.addClass(options.classCollapsed)
-				.wrapInner('<a href="javascript:;" />')
-				.each(function(){ 
-					/*
-					**
-					** Wrap everything after the “default.element” (<H2> by default)
-					** Adds the class used to toggle show/hide.
-					*/
-				    $(this)
-				    	.nextUntil(options.element)
-				    	.wrapAll('<div class="'+options.classContent+'" />');
-				    /*
-				    **	
-				    ** click to open. Toggle class names. Use :after or 
-				    ** background images in your CSS to give visual input.
-				    */
-				    $(this).find('a').on('click touchstart', function(e) {
-				    	$(this)
-				    		/*
-				    		**	
-				    		** Toggle class on “default.element” to show/hide.
-				    		*/
-				    		.parent()
-				    			.toggleClass(options.classCollapsed)
-				    			.toggleClass(options.classExpanded);
-				    		/*
-				    		**	
-				    		** Toggle class on wrapped content to show/hide.
-				    		*/
-				    	e.preventDefault();
-				    });
-				});
-		});
+		/*
+		**
+		** Self invoking function, checks viewport size.
+		*/
+		var viewPort = function() {
+			var w = $(window).width();
+			var h = $(window).height(); 
+			return {width:w, height:h};
+		}();
+		/*
+		**
+		** Init only at certain size.
+		** For example, if you only wanna fold content on 
+		*/
+		if(viewPort.width<=options.initSize || options.initSize==0) {
+			return this.each(function() { 
+				/*
+				**
+				** wrapper to start from, find element, add class for collapsed content.
+				** for all siblings following the element, we wrap it in a div-element.
+				*/
+				$(this)
+					.find(options.element)
+					.addClass(options.classCollapsed)
+					.wrapInner('<a href="javascript:;" />')
+					.each(function(){ 
+						/*
+						**
+						** Wrap everything after the “default.element” (<H2> by default)
+						** Adds the class used to toggle show/hide.
+						*/
+					    $(this)
+					    	.nextUntil(options.element)
+					    	.wrapAll('<div class="'+options.classContent+'" />');
+					    /*
+					    **	
+					    ** click to open. Toggle class names. Use :after or 
+					    ** background images in your CSS to give visual input.
+					    */
+					    $(this).find('a').on('click touchstart', function(e) {
+					    	$(this)
+					    		/*
+					    		**	
+					    		** Toggle class on “default.element” to show/hide.
+					    		*/
+					    		.parent()
+					    			.toggleClass(options.classCollapsed)
+					    			.toggleClass(options.classExpanded);
+					    		/*
+					    		**	
+					    		** Toggle class on wrapped content to show/hide.
+					    		*/
+					    	e.preventDefault();
+					    });
+					});
+			});
+		}
 	}
 })(jQuery);
